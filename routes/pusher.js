7bad72a5ba779a765354837539861754a0b5bbcc
Glenn Dimaliwat <glenn.dimaliwat@gmail.com>
2018-01-10 13:43:27 +1100

    Add pushMessage function

--- routes/pusher.js
+++ routes/pusher.js
@@ -1,12 +1,16 @@
 const express = require('express')
 const router = express.Router()
+const { pushMessage } = require('../pusher/pusherClient')
 
 router.post('/push', (req, res) => {
-  
-  // .then(data => {
-  //   res.status(201).json({ data })
-  // })
-  // .catch(error => {
-  //   res.status(400).json({ error })    
-  // })
+  const attributes = req.body
+  pushMessage(attributes.message)
+    .then(data => {
+      res.status(201).json({ data })
+    })
+    .catch(error => {
+      res.status(400).json({ error })    
+    })
 })
+
+module.exports = router
